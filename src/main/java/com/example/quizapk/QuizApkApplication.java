563391b1e0d662b110fb8372cd862157be67fb2f
Yahya AFADISSE <yahya.afadisse@enova.ma>
2026-06-08 23:04:28 +0100

    first commit

---
+++ src/main/java/com/example/quizapk/QuizApkApplication.java
@@ -0,0 +1,13 @@
+package com.example.quizapk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizApkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizApkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/quizapk/QuizApkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quizapk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizApkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
